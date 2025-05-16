90e62c2a4dbeab40651ae345ec5ea2ec44427a1e
Jacob Butters <jacobbutters@Jacobs-MacBook-Pro.local>
2025-05-16 09:12:33 -0600

    update resume link

--- src/Components/About.js
+++ src/Components/About.js
@@ -12,7 +12,7 @@ const About = () => {
           <p>I have been coding ever since. I thrive on creating simple yet practical web applications to make life more efficient and better.</p>
           <p>In my free time I hike with my wife, play chess, and spend time with my daughter.</p>
         </div>
-        <a href='https://docs.google.com/document/d/1i2whV3E6rlYfMxYATagTWbZwanz4ZY67IvGcVhw27rM/edit?usp=sharing' className='computer-resume'>
+        <a href='https://docs.google.com/document/d/1kFXUj6DV2Syv6kkm7Q2VbvLHAg0V4fXOsAibcVlxkZ0/edit?usp=sharing' className='computer-resume'>
           <img src={computer} alt='computer coding icon' className='computer-icon' />
           <h4>My Resume</h4>
         </a>
